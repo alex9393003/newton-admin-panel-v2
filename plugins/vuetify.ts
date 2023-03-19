6639c5268123d4617b1fbce66554db1c4bb0f2b9
Mitchell Thomas <mt@mt.codes>
2023-03-19 17:30:47 -0400

    initial commit

---
+++ plugins/vuetify.ts
@@ -0,0 +1,25 @@
+import { createVuetify, ThemeDefinition } from "vuetify";
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives'
+
+const myTheme: ThemeDefinition = {
+    colors: {
+        primary: '#EAEAEA'
+    }
+}
+
+export default defineNuxtPlugin(nuxtApp => {
+    const vuetify = createVuetify({
+        components,
+        directives,
+        theme: {
+            // themes: {
+
+            // }
+            defaultTheme: 'dark'
+        }
+    })
+
+    nuxtApp.vueApp.use(vuetify);
+});
+
