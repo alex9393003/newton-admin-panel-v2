823573c448146ecbcbb65de2f7eaf4c1122f4287
Mitchell Thomas <mt@mt.codes>
2023-04-30 21:32:18 -0400

    debug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,12 @@
 export default defineNuxtConfig({
     css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
     build: {
-        transpile: ['vuetify', '@vuepic/vue-datepicker', 'chart.js']
+        transpile: ['vuetify', '@vuepic/vue-datepicker', 'chart.js', '@/server/typeorm/entity/User',
+        '@/server/typeorm/entity/Note',
+        '@/server/typeorm/entity/Patient',
+        '@/server/typeorm/entity/Entry',]
     },
+    
     
     plugins: [
         '~/plugins/firebaseAuth.ts', 
@@ -38,7 +42,6 @@ export default defineNuxtConfig({
     modules: [
         '@pinia/nuxt',
     ],
-    
     pinia: {
         autoImports: ['defineStore', 'acceptHMRUpdate'],
     },
