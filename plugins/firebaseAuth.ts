dd8a72b56fbe3fadfb5490fa2fa6ca6e0be7f17d
Alex <alexandrosavgoustiss@gmail.com>
2024-08-15 00:15:29 +0300

    Update firebaseAuth.ts

--- plugins/firebaseAuth.ts
+++ plugins/firebaseAuth.ts
@@ -7,14 +7,14 @@ export default defineNuxtPlugin((nuxtApp) => {
     // Your web app's Firebase configuration
     // create below firebase config again but with config.public
 
-    const firebaseConfig = {
-        apiKey: config.public.FIREBASE_API_KEY,
-        authDomain: config.public.FIREBASE_AUTH_DOMAIN,
-        projectId: config.public.FIREBASE_PROJECT_ID,
-        storageBucket: config.public.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: config.public.FIREBASE_MESSAGING_SENDER_ID,
-        appId: config.public.FIREBASE_APP_ID,
-    };
+const firebaseConfig = {
+  apiKey: "AIzaSyC5rkN0RF93_q1uYgoGQF7ex5TFryaussA",
+  authDomain: "lolt-bb9aa.firebaseapp.com",
+  projectId: "lolt-bb9aa",
+  storageBucket: "lolt-bb9aa.appspot.com",
+  messagingSenderId: "632100441282",
+  appId: "1:632100441282:web:4dc8d680f755e7d0baff5b"
+};
 
     // const firebaseConfig = {
     //     apiKey: "AIzaSyAW9gqgxAIw7tUhc4vaTR72mI13E3HsxOg",
